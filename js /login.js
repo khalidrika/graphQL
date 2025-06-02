8ae7dd9ce2dd271e655edafd7fb6dec7541c9267
ktrichin <rikarikamika2002@gmail.com>
2025-06-02 14:01:12 +0100

    login

---
+++ js /login.js
@@ -0,0 +1,37 @@
+let usernameInput = document.querySelector('.username');
+let passwordInput = document.querySelector('.password');
+let showPasswordButton = document.querySelector('.password-button');
+let face = document.querySelector('.face');
+
+passwordInput.addEventListener('focus', () => {
+    document.querySelectorAll('.hand').forEach(hand => hand.classList.add('hide'));
+    document.querySelector('.tongue').classList.remove('breath');
+});
+
+passwordInput.addEventListener('blur', () => {
+    document.querySelectorAll('.hand').forEach(hand => hand.classList.remove('hide', 'peek'));
+    document.querySelector('.tongue').classList.add('breath');
+});
+
+usernameInput.addEventListener('focus', () => {
+    let length = Math.min(usernameInput.value.length - 16, 19);
+    face.style.setProperty('--rotate-head', `${-length}deg`);
+});
+
+usernameInput.addEventListener('blur', () => {
+    face.style.setProperty('--rotate-head', '0deg');
+});
+
+usernameInput.addEventListener('input', event => {
+    let length = Math.min(event.target.value.length - 16, 19);
+    face.style.setProperty('--rotate-head', `${-length}deg`);
+});
+
+showPasswordButton.addEventListener('click', () => {
+    const type = passwordInput.type === 'text' ? 'password' : 'text';
+    passwordInput.type = type;
+    document.querySelectorAll('.hand').forEach(hand => {
+        hand.classList.toggle('hide', type === 'password');
+        hand.classList.toggle('peek', type === 'text');
+    });
+});
--- js /main.js
+++ js /main.js
@@ -1,39 +1,39 @@
 
-function formlogin() {
-    const element = document.getElementById("app")
+// function formlogin() {
+//     const element = document.getElementById("app")
 
-    const divlogin = document.createElement('div')
-    divlogin.className = "login"
+//     const divlogin = document.createElement('div')
+//     divlogin.className = "login"
 
-    element.appendChild(divlogin)
+//     element.appendChild(divlogin)
 
-    const formlogin = document.createElement('form')
-    formlogin.className = "formlogin"
+//     const formlogin = document.createElement('form')
+//     formlogin.className = "formlogin"
 
-    divlogin.appendChild(formlogin)
+//     divlogin.appendChild(formlogin)
 
 
-    const welcome = document.createElement('h2')
-    welcome.className = "welcome"
-    welcome.textContent = "login"
+//     const welcome = document.createElement('h2')
+//     welcome.className = "welcome"
+//     welcome.textContent = "login"
 
-    const input = document.createElement('input')
-    input.type = 'text'
-    input.className = "input"
-    input.placeholder = "Enter your email or username"
+//     const input = document.createElement('input')
+//     input.type = 'text'
+//     input.className = "input"
+//     input.placeholder = "Enter your email or username"
 
-    const passwor = document.createElement("input")
-    passwor.type = "password"
-    passwor.placeholder = "Enter you password"
-    passwor.className = "password"
+//     const passwor = document.createElement("input")
+//     passwor.type = "password"
+//     passwor.placeholder = "Enter you password"
+//     passwor.className = "password"
 
-    const button = document.createElement('button')
-    button.className = "submit"
-    button.textContent = "submit"
+//     const button = document.createElement('button')
+//     button.className = "submit"
+//     button.textContent = "submit"
 
-    formlogin.appendChild(welcome)
-    formlogin.appendChild(input)
-    formlogin.appendChild(passwor)
-    formlogin.appendChild(button)
-}
-formlogin();
+//     formlogin.appendChild(welcome)
+//     formlogin.appendChild(input)
+//     formlogin.appendChild(passwor)
+//     formlogin.appendChild(button)
+// }
+// formlogin();
